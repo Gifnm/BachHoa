5da1bb8be1f440d959b1a579694615783a5663ea
vo-nghiep <dongnghiepit@gmail.com>
2023-10-19 10:02:07 +0700

    chuẩn hóa code
    xóa code dư thừa, code xử lý bán hàng bằng angularJS

--- src/main/resources/static/js/addTab.js
+++ src/main/resources/static/js/addTab.js
@@ -1,46 +1,49 @@
+var app = angular.module("app", ["ui.bootstrap", "ui.tab.scroll"]);
 
-function xoa() {
-    document.getElementById('hoadon1').innerHTML = "";
-    document.getElementById('hoadon').innerHTML = "";
-}
-var app = angular.module("app", []);
-app.controller("ctrl", function ($scope) {
-    alert("hello")
-    $scope.newPanel = function () {
-        document.getElementById('myTab').innerHTML
-            += '<li class="nav-item" role="presentation" style="position: relative; width: 130px;">'
-            + '<a class="nav-link" id="new-tab" data-bs-toggle="tab"'
-            + 'data-bs-target="#hoadon" skip-disable role="tab" aria-controls="new-tab" aria-selected="false">Hóa đơn 1'
-            + '</a>'
-            + '<button type = "button" class="btn-close" aria-label="Close"'
-            + 'style="position: absolute; top: 0; right: 0; padding: 8px;"></button>'
-            + '</li>';
-        document.getElementById('myTabContent').innerHTML
-            += '<div class="tab-pane fade table-responsive" id="hoadon" role="tabpanel"'
-            + 'aria-labelledby="new-tab">'
-            + '<table class="table">'
-            + '<thead>'
-            + '<tr>'
-            + '<th scope="col">Mã sản phẩm</th>'
-            + '<th scope="col">Tên sản phẩm</th>'
-            + '<th scope="col">Đơn vị</th>'
-            + '<th scope="col">Số lượng</th>'
-            + '<th scope="col">Đơn giá</th>'
-            + '<th scope="col">Thành tiền</th>'
-            + '<th scope="col">Bỏ</th>'
-            + '</tr>'
-            + '</thead>'
-            + '<tbody>'
-            + '<tr>'
-            + '<td>1</td>'
-            + '<td>PESSI</td>'
-            + '<td>Chai</td>'
-            + '<td>5</td>'
-            + '<td>20000</td>'
-            + '<td>100000</td>'
-            + '</tr>'
-            + '</tbody>'
-            + '</table>'
-            + '</div>';
-    };
+app.controller("add-tab-ctrl", function ($scope) {
+  $scope.invoices = [{ id: 1, name: "Hóa đơn 1", active: true }];
+
+  var id = 1;
+
+  var addNewBill = function () {
+    alert("run1")
+    if ($scope.invoices.length == 0) {
+      // var id = $scope.bills.length + 1;
+      id = 1;
+      $scope.invoices.push({
+        id: id,
+        name: "Hóa đơn " + id,
+        active: true,
+      });
+    } else {
+      id = id + 1;
+      var id_next = id;
+      $scope.invoices.unshift({
+        id: id_next,
+        name: "Hóa đơn " + id_next,
+        active: true,
+      });
+    }
+  };
+
+  var setAllInactive = function () {
+    angular.forEach($scope.invoices, function (bill) {
+      bill.active = false;
+    });
+  };
+
+  $scope.addBill = function () {
+    setAllInactive();
+    addNewBill();
+  };
+
+  $scope.removeTab = function (index) {
+    $scope.invoices.splice(index, 1);
+    if ($scope.invoices.length == 0) {
+      addNewBill();
+    }
+  };
+
 });
+
+
