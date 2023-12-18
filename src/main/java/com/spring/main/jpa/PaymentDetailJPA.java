0283a2cdad99b4899781da3fbbdb7d26d46c99d4
Nguyen Minh Thu <98570646+Gifnm@users.noreply.github.com>
2023-12-19 00:29:13 +0700

    Revert "Merge remote-tracking branch 'origin/vo-nghiep'"
    This reverts commit 30e3755cc4662dc1f4fbd5585287d6ce4d76ffff, reversing
    changes made to 5c3af2d1c3729e594a23eca909a889885619779f.

--- src/main/java/com/spring/main/jpa/PaymentDetailJPA.java
+++ src/main/java/com/spring/main/jpa/PaymentDetailJPA.java
@@ -2,10 +2,11 @@ package com.spring.main.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import com.spring.main.model.PaymentHistory;
 
-public interface PaymentDetailJPA extends JpaRepository<PaymentHistory, Integer>{
+import com.spring.main.model.PaymentDetail;
 
-	@Query("select o from PaymentHistory o where o.id = ?1")
-	PaymentHistory findByPaymentID(Integer id);
+public interface PaymentDetailJPA extends JpaRepository<PaymentDetail, Integer>{
+
+	@Query("select o from PaymentDetail o where o.paymentHistory.id = ?1")
+	PaymentDetail findByPaymentID(Integer id);
 }
